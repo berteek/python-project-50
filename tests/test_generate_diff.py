d5b02a5cb126056032724ddb914f5d851826841a
berteek <merzinalbert@gmail.com>
2023-03-30 23:34:22 +0400

    feat: update tests

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,7 +1,25 @@
 from gendiff.diff import generate_diff
+import pytest
 
 
-def test_generate_diff():
-    expected = open('./tests/fixtures/json/expected1.txt').read().rstrip('\n')
+@pytest.fixture
+def diff():
+    return open('./tests/fixtures/expected1.txt').read().rstrip('\n')
+
+
+def test_works_with_json(diff):
+    expected = diff
     actual = generate_diff('./tests/fixtures/json/file1.json', './tests/fixtures/json/file2.json')
+    assert expected == actual
+
+
+def test_works_with_yaml(diff):
+    expected = diff
+    actual = generate_diff('./tests/fixtures/yaml/file1.yml', './tests/fixtures/yaml/file2.yaml')
+    assert expected == actual
+
+
+def test_works_with_mixed(diff):
+    expected = diff
+    actual = generate_diff('./tests/fixtures/json/file1.json', './tests/fixtures/yaml/file2.yaml')
     assert expected == actual
